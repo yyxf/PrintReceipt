323ed58152133a50c93733f1b5a6521b3ff53466
yyxf <1456425658@qq.com>
2019-01-22 09:44:50 +0800

    js实现打印收据

---
+++ src/main/java/com/pxz/print/receipt/printreceipt/PrintreceiptApplication.java
@@ -0,0 +1,14 @@
+package com.pxz.print.receipt.printreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintreceiptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintreceiptApplication.class, args);
+    }
+
+}
+
